51c19713c37804870059e69560f638edd4eeb1d6
cael170 <cael170@abv.bg>
2024-11-23 00:05:18 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using TeaManagmentSystem2.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<TeaManagementContext>(options =>
+    options.UseInMemoryDatabase("TeaManagementDB"));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
